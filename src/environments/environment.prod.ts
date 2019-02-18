47ce6b1f8a3844e767febb8614b69e229e6682d8
Anchortek <jake@gamebench.net>
2019-02-19 00:51:21 +0100

    Set default port to 4321 and host to 0.0.0.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://localhost:3000",
+  apiUrl: "https://revscrp.backend.gbdev.tech",
   hsFormsApiUrl: "https://api.hsforms.com/submissions/v3/integration/submit/2891910/c4146edc-1cf5-4e35-bdf4-96996c859f0a"
 };
