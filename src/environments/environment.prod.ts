adee94369f35020f5fc0a7a76c4df8774ab2a901
kjsuarez <kjsuarez94@gmail.com>
2019-03-02 21:16:24 -0800

    changed production server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://revscrp.backend.gbdev.tech",
+  apiUrl: "https://review-parser.herokuapp.com",
   hsFormsApiUrl: "https://api.hsforms.com/submissions/v3/integration/submit/2891910/c4146edc-1cf5-4e35-bdf4-96996c859f0a"
 };
